3977b67de9c2b30f6214aa4cdbe9189dfd465fe8
Sajal Sangal <sajalsangal@gmail.com>
2025-05-19 10:46:24 +0530

    Changed parameter name for findHands from flippedimage to image

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -15,15 +15,15 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
 
 
-    def findHands(self, flipped_image, draw = True):
-        imgRGB = cv2.cvtColor(flipped_image, cv2.COLOR_BGR2RGB)
+    def findHands(self, image, draw = True):
+        imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
 
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-                    self.mpDraw.draw_landmarks(flipped_image, handLms, self.mpHands.HAND_CONNECTIONS)
-        return flipped_image
+                    self.mpDraw.draw_landmarks(image, handLms, self.mpHands.HAND_CONNECTIONS)
+        return image
 
     def findPosition(self, flipped_image, handNo = 0, draw = True):
         lmList = []
