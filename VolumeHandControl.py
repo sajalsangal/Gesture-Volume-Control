c07a4031074c557149e8ee3c5b831aa967cd92f1
Sajal Sangal <sajalsangal@gmail.com>
2025-05-19 10:47:05 +0530

    basic cv2 video object created and fps added

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,11 +1,22 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import time
 
 cap = cv2.VideoCapture(0)
+wCam, hCam = 640, 480
+cap.set(3, wCam)
+cap.set(4, hCam)
+pTime = 0
 
 while True:
     success,img = cap.read()
 
-    cv2.imshow("Image", img)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    flipped_img = cv2.flip(img,1)
+    cv2.putText(flipped_img, f'FPS: {str(int(fps))}', (40,50), cv2.FONT_HERSHEY_SIMPLEX, 1
+                , (255, 0, 0) , 3)
+    cv2.imshow("Image", flipped_img)
     cv2.waitKey(1)
